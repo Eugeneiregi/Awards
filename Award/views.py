ccd6ab907e8b7f1dfc046425dc4552e300a42455
virginiah894 <virgyperry@gmail.com>
2020-06-09 15:58:12 +0300

    templates

--- Award/views.py
+++ Award/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.urls import path
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 
 
 
